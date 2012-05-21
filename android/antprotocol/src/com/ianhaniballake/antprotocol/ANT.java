44eab63478e55b86a2ddead2e1c18f23c0a7e99d
Ian Lake <ian.hannibal.lake@gmail.com>
2012-05-20 21:51:05 -0700

    Adds Fitbit init sequence
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

--- android/antprotocol/src/com/ianhaniballake/antprotocol/ANT.java
+++ android/antprotocol/src/com/ianhaniballake/antprotocol/ANT.java
@@ -1,18 +1,70 @@
 package com.ianhaniballake.antprotocol;
 
+import android.hardware.usb.UsbConstants;
 import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
 
 public class ANT
 {
+	private static final int READ_TIMEOUT = 1000;
+	private static final int WRITE_TIMEOUT = 100;
 	private final UsbDeviceConnection connection;
+	private final UsbInterface intf;
+	private final UsbEndpoint mEndpointIn;
+	private final UsbEndpoint mEndpointOut;
 
-	public ANT(final UsbDeviceConnection connection)
+	public ANT(final UsbDeviceConnection connection, final UsbInterface intf)
 	{
 		this.connection = connection;
+		this.intf = intf;
+		UsbEndpoint epOut = null;
+		UsbEndpoint epIn = null;
+		// look for our bulk endpoints
+		for (int i = 0; i < intf.getEndpointCount(); i++)
+		{
+			final UsbEndpoint ep = intf.getEndpoint(i);
+			if (ep.getType() == UsbConstants.USB_ENDPOINT_XFER_BULK)
+				if (ep.getDirection() == UsbConstants.USB_DIR_OUT)
+					epOut = ep;
+				else
+					epIn = ep;
+		}
+		if (epOut == null || epIn == null)
+			throw new IllegalArgumentException("not all endpoints found");
+		mEndpointOut = epOut;
+		mEndpointIn = epIn;
 	}
 
 	public void closeConnection()
 	{
+		connection.releaseInterface(intf);
 		connection.close();
+	}
+
+	public void controlTransfer(final int requestType, final int request,
+			final int value, final int index, final byte[] buffer)
+	{
+		connection.controlTransfer(requestType, request, value, index, buffer,
+				buffer.length, ANT.WRITE_TIMEOUT);
+	}
+
+	public void controlTransfer(final int requestType, final int request,
+			final int value, final int index, final int length)
+	{
+		connection.controlTransfer(requestType, request, value, index, null,
+				length, ANT.WRITE_TIMEOUT);
+	}
+
+	public int read(final byte[] data, final int length)
+	{
+		return connection.bulkTransfer(mEndpointIn, data, length,
+				ANT.READ_TIMEOUT);
+	}
+
+	public int write(final byte[] buffer)
+	{
+		return connection.bulkTransfer(mEndpointOut, buffer, buffer.length,
+				ANT.WRITE_TIMEOUT);
 	}
 }
--- android/fitbit/src/com/ianhanniballake/fitbit/Fitbit.java
+++ android/fitbit/src/com/ianhanniballake/fitbit/Fitbit.java
@@ -11,9 +11,7 @@ public class Fitbit
 {
 	private static final int INTERFACE_INDEX = 0;
 	private ANT ant = null;
-	private UsbDeviceConnection connection;
 	private final UsbDevice device;
-	private UsbInterface intf;
 	private final UsbManager manager;
 
 	public Fitbit(final UsbManager manager, final UsbDevice device)
@@ -24,19 +22,35 @@ public class Fitbit
 
 	public void close()
 	{
-		connection.releaseInterface(intf);
 		if (ant != null)
 			ant.closeConnection();
 		ant = null;
 	}
 
+	public void init()
+	{
+		ant.controlTransfer(0x40, 0x00, 0xFFFF, 0x00, new byte[0]);
+		ant.controlTransfer(0x40, 0x01, 0x2000, 0x00, new byte[0]);
+		ant.controlTransfer(0x40, 0x00, 0x00, 0x00, new byte[0]);
+		ant.controlTransfer(0x40, 0x00, 0xFFFF, 0x00, new byte[0]);
+		ant.controlTransfer(0x40, 0x01, 0x2000, 0x00, new byte[0]);
+		ant.controlTransfer(0x40, 0x01, 0x4A, 0x00, new byte[0]);
+		ant.controlTransfer(0xC0, 0xFF, 0x370B, 0x00, 1);
+		ant.controlTransfer(0x40, 0x03, 0x800, 0x00, new byte[0]);
+		ant.controlTransfer(0x40, 0x13, 0x00, 0x00, new byte[] { 0x08, 0x00,
+				0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00 });
+		ant.controlTransfer(0x40, 0x12, 0x0C, 0x00, new byte[0]);
+		ant.read(new byte[4096], 4096);
+	}
+
 	public boolean open()
 	{
-		intf = device.getInterface(Fitbit.INTERFACE_INDEX);
-		connection = manager.openDevice(device);
+		final UsbInterface intf = device.getInterface(Fitbit.INTERFACE_INDEX);
+		final UsbDeviceConnection connection = manager.openDevice(device);
 		final boolean claimed = connection.claimInterface(intf, true);
 		if (claimed)
-			ant = new ANT(connection);
+			ant = new ANT(connection, intf);
 		return claimed;
 	}
 }
